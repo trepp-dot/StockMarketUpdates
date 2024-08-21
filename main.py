af1d84796b6018ced3e2e72614267c59f6d4fba2
trepp <trepp49@gmai.com>
2024-08-21 21:05:47 +0300

    Change methods to class  save PNG's to GitHub Artifact while in process

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from methods import *
 
 
 if __name__ == "__main__":
-    for stock, desc in c.STOCKS_ETF.items():
-        stock_data = fetch_stock_data(symbol=stock, use_local_data=True)
-        generate_graph(stock, desc,  stock_data)
-
-    send_email_with_attachment()
+    # Fetch and save stock data
+    stock_process = StockProcess(c.STOCKS_ETF)
+    stock_process.date_graph_process()
+    stock_process.send_email_with_attachment()
